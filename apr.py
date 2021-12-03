beefbb6fca232b24e8037c48005d93afec6e5d08
Grandi <grandirose@protonmail.com>
2021-12-03 15:50:53 -0500

    update addresses

--- apr.py
+++ apr.py
@@ -14,23 +14,23 @@ lpAddresses = {
     },
     # upadate the addresses of the rest
     "Frax Farm": {
-        "deposited_token_address": "0xfF79D5bff48e1C01b722560D6ffDfCe9FC883587",
-        "farm_address": "0x52CACa9a2D52b27b28767d3649565774A3B991f3",
+        "deposited_token_address": "0xbB5279353d88A25F099A334Ba49CDCb1CF4b5A7c",
+        "farm_address": "0x7b359Af630a195C05Ac625D261aEe09a69aF7744",
         "this_months_rewards": 1000.00
     },
     "stRose Farm": {
-        "deposited_token_address": "0xfF79D5bff48e1C01b722560D6ffDfCe9FC883587",
-        "farm_address": "0x52CACa9a2D52b27b28767d3649565774A3B991f3",
+        "deposited_token_address": "0x7Ba8C17010a48283D38a4bd5f87EfEB5594c92f8",
+        "farm_address": "0x247c9DA96BfC4720580ee84E01566D79a8c901ca",
         "this_months_rewards": 1000.00
     },
     "ROSE/FRAX NLP Farm": {
-        "deposited_token_address": "0xfF79D5bff48e1C01b722560D6ffDfCe9FC883587",
-        "farm_address": "0x52CACa9a2D52b27b28767d3649565774A3B991f3",
+        "deposited_token_address": "0xeD4C231b98b474f7cAeCAdD2736e5ebC642ad707",
+        "farm_address": "0x1B10bFCd6192edC573ced7Db7c7e403c7FAb8068",
         "this_months_rewards": 1000.00
     },
-    "stRose Farm": {
-        "deposited_token_address": "0xfF79D5bff48e1C01b722560D6ffDfCe9FC883587",
-        "farm_address": "0x52CACa9a2D52b27b28767d3649565774A3B991f3",
+    "ROSE/PAD NLP Farm": {
+        "deposited_token_address": "0xC6C3cc84EabD4643C382C988fA2830657fc70a6B",
+        "farm_address": "0x9b2aE7d53099Ec64e2f6df3B4151FFCf7205f788",
         "this_months_rewards": 1000.00
     },
 }
@@ -67,6 +67,7 @@ for farmName, payload in lpAddresses.items():
     data.append({
         farmName: {
             "deposited_token_address": payload["deposited_token_address"],
+            "farm_address": payload["farm_address"],
             "farm_tvl": str(farmTvl),
             "apr": apr,
         }
