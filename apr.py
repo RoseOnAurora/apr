3b4f52defe3b208b773a691a2f045c86d6e9c5d4
Grandi <grandirose@protonmail.com>
2021-12-04 17:45:06 -0500

    update rewards amounts

--- apr.py
+++ apr.py
@@ -13,7 +13,7 @@ lpAddresses = {
         "deposited_token_address": "0xfF79D5bff48e1C01b722560D6ffDfCe9FC883587",
         "farm_address": "0x52CACa9a2D52b27b28767d3649565774A3B991f3",
         "pool_address": "0xc90dB0d8713414d78523436dC347419164544A3f",
-        "this_months_rewards": 1000.00
+        "this_months_rewards": 450275.00
     },
     "Frax Farm": {
         "deposited_token_address": "0xbB5279353d88A25F099A334Ba49CDCb1CF4b5A7c",
@@ -30,12 +30,12 @@ lpAddresses = {
     "ROSE/FRAX NLP Farm": {
         "deposited_token_address": "0xeD4C231b98b474f7cAeCAdD2736e5ebC642ad707",
         "farm_address": "0x1B10bFCd6192edC573ced7Db7c7e403c7FAb8068",
-        "this_months_rewards": 1000.00
+        "this_months_rewards": 225137.00
     },
     "ROSE/PAD NLP Farm": {
         "deposited_token_address": "0xC6C3cc84EabD4643C382C988fA2830657fc70a6B",
         "farm_address": "0x9b2aE7d53099Ec64e2f6df3B4151FFCf7205f788",
-        "this_months_rewards": 1000.00
+        "this_months_rewards": 525321.00
     },
 }
 data = []
@@ -44,13 +44,17 @@ w3 = Web3(Web3.HTTPProvider("https://mainnet.aurora.dev/"))
 
 # get price of ROSE against FRAX
 rosefraxpool = init_rosefraxpool(w3)
+rose = init_token(w3, "0xdcD6D4e2B3e1D1E1E6Fa8C21C8A323DcbecfF970")
 roseprice = 0
 try:
     roseprice = rosefraxpool.functions.price0CumulativeLast().call()
+    # stroseBalance = 
 except:
     print("Error getting price of ROSE against FRAX")
 
-rose_data.append({"price_of_rose": str(roseprice)})
+rose_data.append({
+    "price_of_rose": str(roseprice)
+})
 
 # fetch apr for each farm
 for farmName, payload in lpAddresses.items():
